7b9c5387d658fac69d882893c0491283864da0a8
RomanK <romiki44@hotmail.com>
2022-06-12 15:39:17 +0200

    Shopping Aggregator done

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly ICatalogService catalogService;
+        private readonly IBasketService basketService;
+        private readonly IOrderService orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService, 
+            IOrderService orderService)
+        {
+            this.catalogService = catalogService;
+            this.basketService = basketService;
+            this.orderService = orderService;
+        }
+
+        [HttpGet("{userName}", Name = "GetShopping")]
+        [ProducesResponseType(typeof(ShoppingModel), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+        {
+            // get basket with username
+            // iterate basket items and consume products with basket item productId member
+            // map product related members into basketitem dto with extended columns
+            // consume ordering microservices in order to retrieve order list
+            // return root ShoppngModel dto class which including all responses
+
+            var basket = await basketService.GetBasket(userName);
+
+            foreach (var item in basket.Items)
+            {
+                var product = await catalogService.GetCatalog(item.ProductId);
+
+                // set additional product fields onto basket item
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders = await orderService.GetOrdersByUserName(userName);
+
+            var shoppingModel = new ShoppingModel
+            {
+                UserName = userName,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+
+            return Ok(shoppingModel);
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,21 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient client;
+
+        public BasketService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await client.GetAsync($"/api/v1/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+    }
+}
